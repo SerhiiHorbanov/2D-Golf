2e4bea22a3a5cf2926259b41fdf4b6e1d0100c1f
Serhii <serikgorbanev@gmail.com>
2023-05-14 10:30:09 +0200

    Deleted enums folder. Added CheckBoundsCollisions method to GolfBall

--- 2D-Golf/Scripts/GolfBall.cs
+++ 2D-Golf/Scripts/GolfBall.cs
@@ -11,10 +11,11 @@ namespace Golf_2D
 {
     class GolfBall
     {
-        int radius;
-        Vector2f position;
-        Vector2f velocity;
-        CircleShape shape;
+        private static float velocityReduce = 0.99f;
+        private int radius;
+        private Vector2f position;
+        private Vector2f velocity;
+        private CircleShape shape;
 
         public GolfBall(int radius, Vector2f position)
         {
@@ -25,7 +26,34 @@ namespace Golf_2D
 
         public void Update()
         {
+            velocity *= velocityReduce;
             position += velocity;
+
+            CheckBoundsCollisions();
+        }
+
+        private void CheckBoundsCollisions()
+        {
+            if (position.X + radius > Golf2D.window.Size.X)
+            {
+                position.X = Golf2D.window.Size.X - radius;
+                velocity.X = -velocity.X;
+            }
+            if (position.X > radius)
+            {
+                position.X = radius;
+                velocity.X = -velocity.X;
+            }
+            if (position.Y + radius > Golf2D.window.Size.Y)
+            {
+                position.Y = Golf2D.window.Size.Y - radius;
+                velocity.Y = -velocity.Y;
+            }
+            if (position.Y > radius)
+            {
+                position.Y = radius;
+                velocity.Y = -velocity.Y;
+            }
         }
 
         public void Render()
